359590cf4e4077a04f3b00c946a9f3a57db0381d
keenankl <kevin.l.keenan@gmail.com>
2018-09-09 19:49:38 -0400

    initial commit

---
+++ src/main/java/edu/keenank/advancedjava/App.java
@@ -0,0 +1,13 @@
+package edu.keenank.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
