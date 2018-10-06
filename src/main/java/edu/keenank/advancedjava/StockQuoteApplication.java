f507840a5aed887dd5ce5b7e1586ed10526ee444
keenankl <kevin.l.keenan@gmail.com>
2018-10-06 19:58:15 -0400

    fixed application and intervalenum

---
+++ src/main/java/edu/keenank/advancedjava/StockQuoteApplication.java
@@ -0,0 +1,46 @@
+package edu.keenank.advancedjava;
+
+import edu.keenank.advancedjava.services.StockService;
+import edu.keenank.advancedjava.services.StockServiceException;
+import edu.keenank.advancedjava.services.StockServiceFactory;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Application class with a main method to create StockService instances
+ */
+@Immutable
+public class StockQuoteApplication {
+    private StockService basicStockService;
+
+    public StockQuoteApplication(StockService basicStockService) {
+        this.basicStockService = basicStockService;
+    }
+
+    /**
+     * @param args user input that contains elements:
+     * <code>String</code>  stock symbol, start date, end date, and interval
+     * Prints out the results
+     */
+    public static void main(String[] args) throws ParseException, StockServiceException {
+        StockService basicStockService = StockServiceFactory.getInstance(ServiceType.BASIC);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+
+        System.out.println(basicStockService.getQuote(args[0]));
+
+        Calendar from = Calendar.getInstance();
+        from.setTime(sdf.parse(args[1]));
+
+        Calendar until = Calendar.getInstance();
+        until.setTime(sdf.parse(args[2]));
+
+        System.out.println(basicStockService.getQuote(args[0], from, until));
+
+        IntervalEnum interval = IntervalEnum.valueOf(args[3]);
+        System.out.println(basicStockService.getQuote(args[0], from, until, interval));
+    }
+
+}
