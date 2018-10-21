5951c34af4fd515da3ee8aa350d019984c7aeee1
keenankl <kevin.l.keenan@gmail.com>
2018-10-21 17:48:12 -0400

    Update src/main/java/edu/keenank/advancedjava/services/XMLObjectFactory.java

--- src/main/java/edu/keenank/advancedjava/services/XMLObjectFactory.java
+++ src/main/java/edu/keenank/advancedjava/services/XMLObjectFactory.java
@@ -11,18 +11,7 @@ import javax.xml.namespace.QName;
 
 
 /**
- * This object contains factory methods for each
- * Java content interface and Java element interface
- * generated in the model package.
- * <p>An XMLObjectFactory allows you to programatically
- * construct new instances of the Java representation
- * for XML content. The Java representation of XML
- * content can consist of schema derived interfaces
- * and classes representing the binding of schema
- * type definitions, element declarations and model
- * groups.  Factory methods for each of these are
- * provided in this class.
- *
+ * Factory class for XML Objects
  */
 @XmlRegistry
 public class XMLObjectFactory implements XMLDomainObject {
@@ -30,7 +19,7 @@ public class XMLObjectFactory implements XMLDomainObject {
     private final static QName _Stocks_QNAME = new QName("", "stocks");
 
     /**
-     * Create a new XMLObjectFactory that can be used to create new instances of schema derived classes for package: mypackage
+     * Create a new XMLObjectFactory
      *
      */
     public XMLObjectFactory() {
